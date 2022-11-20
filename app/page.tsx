d36e94d89a322225063b0f775c453fbc6875fab5
Fran Bosquet <frbosquet@gmail.com>
2022-11-20 20:22:40 +0100

    load info from sheets

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,26 @@
-export default function HomePage() {
+import { Row } from '../pages/api/rows'
+
+const fetchData = async (): Promise<Row[]> => {
+  const res = await fetch('http://localhost:3000/api/rows', {
+    cache: 'no-store',
+  })
+
+  const data = await res.json()
+
+  return data.rows
+}
+
+export default async function HomePage() {
+  const rows = await fetchData()
+
   return (
     <div>
       <h2>Ultimos clientes</h2>
+      <ul>
+        {rows.map((row) => (
+          <li key={row.id}>{row.nombre}</li>
+        ))}
+      </ul>
     </div>
   )
 }
---
+++ pages/api/rows.ts
@@ -0,0 +1,47 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { GoogleSpreadsheet, GoogleSpreadsheetRow } from 'google-spreadsheet'
+import type { NextApiRequest, NextApiResponse } from 'next'
+// TODO: Move info in creeds to env vars
+import creds from '../../sunray-creds.json'
+
+export type Row = {
+  fecha: Date
+  nombre: string
+  dni: string
+  importe: number
+  generado: boolean
+  id: string
+}
+
+type Data = {
+  rows: Row[]
+}
+
+const parseRow = (row: GoogleSpreadsheetRow): Row => ({
+  fecha: new Date(row.Fecha.split('/').reverse().join('/')),
+  nombre: row.Nombre,
+  dni: row.DNI,
+  importe: +row.Importe.replace(/€|\./g, '').replace(',', '.'),
+  generado: row.Generado === 'TRUE',
+  id: row.rowIndex.toString(),
+})
+
+const doc = new GoogleSpreadsheet(process.env.NEXT_APP_SPREADSHEET_ID)
+
+doc.useServiceAccountAuth(creds)
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  if (req.method !== 'GET') throw new Error('Call this endpoint with get')
+
+  await doc.loadInfo()
+
+  const sheet = doc.sheetsByTitle['Clientes']
+
+  const rawRows = await sheet.getRows()
+  const rows = rawRows.map(parseRow)
+
+  res.status(200).json({ rows })
+}
